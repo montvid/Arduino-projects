ba29bfd38df21ece10f566dea7dcd5b1d339f9db
montvid <montvid@gmail.com>
2017-03-01 21:56:04 +0200

    upgrade

--- lesson_02_tweak.ino
+++ lesson_02_tweak.ino
@@ -1,4 +1,3 @@
-int kartai=0;
 void setup()
 {
   // put your setup code here, to run once:
@@ -12,16 +11,18 @@ void loop()
 {
   // put your main code here, to run repeatedly:
   // jei mygtukas neijungtas, dega zalia lemputė
+  // If the button is off show green light
   if (digitalRead(2) == 0)
   {
     digitalWrite(3, 1);
     digitalWrite(4, 0);
     digitalWrite(5, 0);
-    kartai=0;
   }
   // jei mygtukas įjungtas blyksi 2 raudonos lemputės
+  // If the button is on blink 2 red lights
   else
   {
+    int kartai=0;
     while ( digitalRead(2) == 1 )
     {
       digitalWrite(3, 0);
@@ -33,9 +34,13 @@ void loop()
       delay(1000);
       kartai++;
     }
-    // jei lemputės sublyksėjo daugiau nei 2 kartus atleidus mygtuką užsidegs ir degs dvi raudonos lemputės. Jei mažiau tai ims vėl degti tik žalia.
+    // jei lemputės sublyksėjo daugiau nei 2 kartus atleidus mygtuką užsidegs ir degs visos 3 lemputės. Jei mažiau tai ims vėl degti tik žalia.
+    // If the button is on & the 2 red lights are blinking for 3 times or more - turn on all 3 lights on release of the button.
+
     if (kartai > 2)
       {
+        digitalWrite(3, 1);
+        digitalWrite(4, 1);
         digitalWrite(5, 1);
         delay(5000);
       } 
