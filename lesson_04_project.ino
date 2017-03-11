9ef0916fcec11dd932d90eac888d5e95dca2a2f5
montvid <montvid@gmail.com>
2017-03-11 16:17:52 +0200

    Add files via upload

---
+++ lesson_04_project.ino
@@ -0,0 +1,42 @@
+const int greenLEDPin = 9;
+const int blueLEDPin = 10;
+const int redLEDPin = 11;
+const int sensorPin = A0;
+const float baselineTemp = 20.0;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(greenLEDPin, OUTPUT);
+  pinMode(redLEDPin, OUTPUT);
+  pinMode(blueLEDPin, OUTPUT);
+}
+
+void loop()
+{
+  //Calculate temperature from the sensor value
+  int sensorValue = analogRead(sensorPin);
+  float voltage = sensorValue / 1024.0 * 5.0;
+  float temperature = (voltage - .5) * 100;
+  Serial.println(temperature);
+  //Choose LED color for different temperatures
+  if(temperature < baselineTemp)
+  {
+    analogWrite(redLEDPin, 0);
+    analogWrite(greenLEDPin, 0);
+    analogWrite(blueLEDPin, 50);
+  }
+  if(temperature >= baselineTemp)
+  {
+    analogWrite(redLEDPin, 0);
+    analogWrite(greenLEDPin, 150);
+    analogWrite(blueLEDPin, 0);
+  }
+  if(temperature >= baselineTemp + 2)
+  {
+    analogWrite(redLEDPin, 250);
+    analogWrite(greenLEDPin, 0);
+    analogWrite(blueLEDPin, 0);
+  }
+  delay(5000);
+}
